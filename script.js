e8d694f5ac80cc345ccff5cc7485d8ca8a99b5c4
ninizedginidze <nino.zedginidze.1@iliauni.edu.ge>
2025-06-07 16:13:29 +0400

    feat: implement word length comparison function

---
+++ script.js
@@ -0,0 +1,6 @@
+function compareWordLengths(word1, word2) {
+  return word1.length === word2.length;
+}
+
+console.log (compareWordLengths('hello' , 'world'))
+console.log(compareWordLengths('hi', 'world')); 
